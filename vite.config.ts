82e9adca8cd1a4925ba3543c4a155d7061fd6992
Cian Monnin <cianmonnin@gmail.com>
2024-06-28 18:35:27 -0400

    troubleshooting fetch and parse8

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'pnt-test',
+  base: '/pnt-test/',
   build: {
     target: 'esnext' //browsers can handle the latest ES features
   },

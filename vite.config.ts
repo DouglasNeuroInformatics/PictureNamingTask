4320fac8bbff88e19ee84fe6803f262974cd5578
Cian Monnin <cianmonnin@gmail.com>
2024-06-28 18:01:33 -0400

    troubleshooting fetch and parse5

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'pnt-test',
+  base: '/pnt-test/',
   build: {
     target: 'esnext' //browsers can handle the latest ES features
   },

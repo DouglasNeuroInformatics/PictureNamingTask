90999b9277dc50e9fa615bd05958d8a42514863c
Cian Monnin <cianmonnin@gmail.com>
2024-08-02 11:56:16 -0400

    fix: i18n added github pages deployment logic

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -2,19 +2,33 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 
 import { experimentSettings } from "../fetchAndParse";
-//@ts-expect-error the baseUrl is being read in the loadPath
-const baseUrl = import.meta.env.BASE_URL | "";
 
-await i18n.use(Backend).init({
-  backend: {
-    loadPath: "{baseUrl}/locales/{{lng}}/{{ns}}.json",
-  },
-  defaultNS: "translation",
-  fallbackLng: "en",
-  lng: experimentSettings.language,
-  ns: ["translation"],
-  preload: ["en", "fr"],
-});
+
+// For deployment to Github pages
+if (import.meta.env.BASE_URL === '/PictureNamingTask/') {
+  await i18n.use(Backend).init({
+    backend: {
+      loadPath: "/PictureNamingTask/locales/{{lng}}/{{ns}}.json",
+    },
+    defaultNS: "translation",
+    fallbackLng: "en",
+    lng: experimentSettings.language,
+    ns: ["translation"],
+    preload: ["en", "fr"],
+  });
+}
+else {
+  await i18n.use(Backend).init({
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
+    defaultNS: "translation",
+    fallbackLng: "en",
+    lng: experimentSettings.language,
+    ns: ["translation"],
+    preload: ["en", "fr"],
+  });
+}
 
 await i18n.changeLanguage(i18n.resolvedLanguage);
 export default i18n;
